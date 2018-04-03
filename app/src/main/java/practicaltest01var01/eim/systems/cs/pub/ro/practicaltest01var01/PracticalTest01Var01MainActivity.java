c4d03b1325c84adc8440c46a8b167b10fdc2e053
Student User <student@cti.pub.ro>
2018-04-03 18:24:08 +0300

    Initial step

---
+++ app/src/main/java/practicaltest01var01/eim/systems/cs/pub/ro/practicaltest01var01/PracticalTest01Var01MainActivity.java
@@ -0,0 +1,13 @@
+package practicaltest01var01.eim.systems.cs.pub.ro.practicaltest01var01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest01Var01MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var01_main);
+    }
+}
